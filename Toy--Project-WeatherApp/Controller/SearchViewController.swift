e0f65a0338122b860946934a13b70908e3f04fe8
JeongDeokHo <ekdj98@gmail.com>
2022-01-22 17:45:01 +0900

    commit

--- Toy--Project-WeatherApp/Controller/SearchViewController.swift
+++ Toy--Project-WeatherApp/Controller/SearchViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        searchBar.delegate = self
+        self.navigationController?.isNavigationBarHidden = true
+        searchBar.placeholder = "도시 또는 공항 검색"
+        
+        searchBar.searchTextField.backgroundColor = .darkGray
+        searchBar.searchTextField.textColor = .white
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
     }
     
-
-
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
 }
